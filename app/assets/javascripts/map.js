b3ebf752191773cba555917233c4ae831d429300
BYWallace <bwallace90@gmail.com>
2014-10-09 18:28:41 -0400

    Add google geocoding of input and pan to input

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,7 +1,7 @@
 var markersArray = [];
-var inactive = false;
 var appleMapsStyle = [{"featureType":"water","elementType":"geometry","stylers":[{"color":"#a2daf2"}]},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"color":"#f7f1df"}]},{"featureType":"landscape.natural","elementType":"geometry","stylers":[{"color":"#d0e3b4"}]},{"featureType":"landscape.natural.terrain","elementType":"geometry","stylers":[{"visibility":"off"}]},{"featureType":"poi.park","elementType":"geometry","stylers":[{"color":"#bde6ab"}]},{"featureType":"poi","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"poi.medical","elementType":"geometry","stylers":[{"color":"#fbd3da"}]},{"featureType":"poi.business","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"geometry.stroke","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"labels","stylers":[{"visibility":"off"}]},{"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#ffe15f"}]},{"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#efd151"}]},{"featureType":"road.arterial","elementType":"geometry.fill","stylers":[{"color":"#ffffff"}]},{"featureType":"road.local","elementType":"geometry.fill","stylers":[{"color":"black"}]},{"featureType":"transit.station.airport","elementType":"geometry.fill","stylers":[{"color":"#cfb2db"}]}];
-
+var googleMapsAPIKey = "AIzaSyBBC2BrtSuloODCOhMgRugWNqmBgDFWcpc";
+var map;
 
 $(document).ready(function() {
   // Initialize map on load.
@@ -31,25 +31,44 @@ var initialize = function(startingLat, startingLng) {
   };
 
   // Create a new Google map with the options above.
-  var map = new google.maps.Map($("#map-canvas")[0], mapOptions);
+  map = new google.maps.Map($("#map-canvas")[0], mapOptions);
 
-  bindControls(map);
+  bindControls();
 };
 
 // Bind event listeners for search submission
 
-var bindControls = function(map) {
+var bindControls = function() {
   // Find container for search box and bind event on submit.
   var searchContainer = $("#search-container")[0];
   google.maps.event.addDomListener(searchContainer, "submit", function(e) {
     e.preventDefault();
-    search(map);
+    search();
   });
 
-  var searchButton = $("map-search-submit")[0];
-  google.maps.even.addDomListener(searchButton, "click", function(e) {
+  var searchButton = $("#map-search-submit")[0];
+  google.maps.event.addDomListener(searchButton, "click", function(e) {
     e.preventDefault();
-    search(map);
+    search();
+  });
+};
+
+var search = function() {
+  var searchLocation = $("#map-search input").val();
+
+  // POST ajax request to Google geocoder and parse coordinates
+  $.post("https://maps.googleapis.com/maps/api/geocode/json?address=" + searchLocation.split(" ").join("+") + "&key=" + googleMapsAPIKey, function(data) {
+     panToCoordinates(data);
+  });
+
+};
+
+var panToCoordinates = function(data) {
+  var geoCoordinates = data.results[0].geometry.location;
+  map.panTo(geoCoordinates);
+
+  $.post("/search", geoCoordinates, function(data) {
+    console.log(data);
   });
 };
 
